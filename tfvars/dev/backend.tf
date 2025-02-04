f86edde19f95ff0b494d3e754cb203b237c8f966
raju.gaddam <raju.qa32@gmail.com>
2025-02-03 22:49:29 -0500

    arguments example

--- tfvars/dev/backend.tf
+++ tfvars/dev/backend.tf
@@ -1,4 +1,4 @@
-bucket = "82s-tf-remote-state-dev"
-key    = "expense-infra-dev" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+bucket = "82s-tf-remote-state-multiple-env-dev"
+key    = "expense-infra-multiple-env-dev" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
 region = "us-east-1"
-dynamodb_table = "82s-tf-remote-state-dev"
+dynamodb_table = "82s-tf-remote-state-multiple-env-dev"
